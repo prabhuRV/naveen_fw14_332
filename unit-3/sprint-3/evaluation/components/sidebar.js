ddba4097cc6105f92c1c31b4e66a9fddcf947fd9
Naveen-Peddabuddi <nkrishvi1@gmail.com>
2022-01-17 16:41:46 +0530

    update

--- unit-3/sprint-3/evaluation/components/sidebar.js
+++ unit-3/sprint-3/evaluation/components/sidebar.js
@@ -3,5 +3,8 @@ function sidebar() {
 
     // return your html component here
     //Make sure to give input search box id as ""
+
+    return `<input type="text" placeholder="searchItem">
+    <button id='btn'>Submit</button>`
 }
 export default sidebar
--- unit-3/sprint-3/evaluation/scripts/search.js
+++ unit-3/sprint-3/evaluation/scripts/search.js
@@ -1,6 +1,7 @@
 function storeSearchterm(term) {
 
-
+   return `<input type="text" placeholder="searchItem">
+   <button>Submit</button>`
 }
 
 export default storeSearchterm

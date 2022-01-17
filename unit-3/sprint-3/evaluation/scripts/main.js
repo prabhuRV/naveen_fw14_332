cc50f68a6f02d10e48db29258f15ff20300e2892
Naveen-Peddabuddi <nkrishvi1@gmail.com>
2022-01-17 16:24:15 +0530

    new.html

--- unit-3/sprint-3/evaluation/scripts/main.js
+++ unit-3/sprint-3/evaluation/scripts/main.js
@@ -13,13 +13,18 @@ function appendArticles(articles, mai) {
     articles.map( (elem) => {
 
         let div = document.createElement('div');
-
+        div.onclick = ( ()=> {
+            console.log(elem)
+            let da = elem
+           localStorage.setItem('article',JSON.stringify(da))
+           window.location.href = 'news.html'
+        })
         let con = document.createElement('p');
-        con.textContent= elem.content;
+        con.textContent= elem.title;
         
         let desc = document.createElement('p');
-        desc.textContent = elem.description;
-
+        desc.textContent = elem.content;
+        
         let img = document.createElement('img');
         img.src = elem.image;
         img.setAttribute('width','400px')
@@ -31,4 +36,21 @@ function appendArticles(articles, mai) {
 
 }
 
+function showItem(data,hd,co,im){
+
+     
+      let tit = document.createElement('p');
+      tit.textContent= data.title;
+      hd.append(tit);
+
+      let cont = document.createElement('p');
+      cont.textContent = data.content;
+      co.append(cont);
+
+      let img = document.createElement('img');
+      img.src = data.image;
+      im.append(img)
+}
+
 export { apiCall, appendArticles }
+export { showItem}

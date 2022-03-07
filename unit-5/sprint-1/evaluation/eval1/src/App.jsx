ab371f01ff6538ae3a058b9052a14c329e153310
Naveen Peddabuddi <nkrishvi1@gmail.com>
2022-03-07 10:43:29 +0530

    updating

--- unit-5/sprint-1/evaluation/eval1/src/App.jsx
+++ unit-5/sprint-1/evaluation/eval1/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [BooksCount, setBooksCount] = useState(13)
   const [PensCount, setPensCount] = useState(10)
   const [NotebooksCount, setNotebooksCount] = useState(44)
-
+  const [InkpensCount, setInkpensCount] = useState(78)
   const [TotalCount, setTotalCount] = useState(145)
   const handlingbooks = (value)=> {
     setBooksCount(BooksCount+value)
@@ -19,32 +19,35 @@ function App() {
     setNotebooksCount(NotebooksCount+value)
     setTotalCount(TotalCount+value)
   }
-  const hadnlingInk
+  const handlingInkpens =(value) => {
+     setInkpensCount(InkpensCount+value)
+     setTotalCount(TotalCount+value)
+  }
   return (
     <div>
       <div className='items'>
          <span>Books:</span>
          <button className='addBook' onClick={ ()=> handlingbooks(1)}>+</button>
-         <button className='remBook' onClick={()=> handlingbooks(-1)}>-</button>
+         <button className='remBook' onClick={()=> {if(BooksCount >= 1) {handlingbooks(-1)}}}>-</button>
          <span className='totalBooks'>{BooksCount}</span>
       </div>
       <div className='items'>
          <span>Pens:</span>
          <button className='addPen' onClick={()=> handlingpens(1)}>+</button>
-         <button className='remPen' onClick={()=> handlingpens(-1)}>-</button>
+         <button className='remPen' onClick={()=> { if(PensCount >= 1) {handlingpens(-1)}}}>-</button>
          <span className='totalPens'>{PensCount}</span>
       </div>
       <div className='items'>
          <span>Notebooks:</span>
          <button className='addNotebook' onClick={() => handlingNotebooks(1)}>+</button>
-         <button className='remNotebook' onClick={ ()=> handlingNotebooks(-1)}>-</button>
+         <button className='remNotebook' onClick={ ()=> { if(NotebooksCount>=1) {handlingNotebooks(-1)}}}>-</button>
          <span className='totalNotebooks'>{NotebooksCount}</span>
       </div>
       <div className='items'>
          <span>InkPens:</span>
-         <button className='addInkpen'>+</button>
-         <button className='remInkpen'>-</button>
-         <span className='totalInkpens'>1</span>
+         <button className='addInkpen' onClick={()=> handlingInkpens(1)}>+</button>
+         <button className='remInkpen' onClick={()=> { if(InkpensCount>=1){handlingInkpens(-1)}}}>-</button>
+         <span className='totalInkpens'>{InkpensCount}</span>
       </div>
       <div className='total'>{TotalCount}</div>
     </div>

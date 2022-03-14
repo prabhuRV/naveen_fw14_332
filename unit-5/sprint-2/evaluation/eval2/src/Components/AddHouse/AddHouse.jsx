f3f5ff5e7a2821d51ea15dfd32866a19d948495f
Naveen Peddabuddi <nkrishvi1@gmail.com>
2022-03-14 10:17:28 +0530

    writing

---
+++ unit-5/sprint-2/evaluation/eval2/src/Components/AddHouse/AddHouse.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export const AddHouse = () => {
+  const [addhouse, setHouse]  = useState({
+    name:"",
+    ownername:"",
+    address:"",
+    areaCode:"",
+    rent:"",
+    bachelor:"",
+    married:"",
+    image:""
+  })
+  const handleChange = (e)=> {
+    console.log(e.target.value)
+  }
+  return (
+    <div className="addHouseContainer">
+      <form>
+        <label>name</label>
+        <input type="text" className="name" value={""} required onChange={handleChange}/>
+        <br />
+        <label>ownerName</label>
+        <input value={""} type="text" className="ownerName" required />
+        <br />
+        <label>address</label>
+        <input value={""} type="text" className="address" required />
+        <br />
+        <label>areaCode</label>
+        <input value={""} type="text" className="areaCode" required />
+        <br />
+        <label>rent</label>
+        <input value={""} type="text" className="rent" required />
+        <br />
+        <label>preferredTenant</label>
+        <br />
+        <label>bachelor</label>
+        <input checked={""} type="checkbox" className="bachelor" />
+        <br />
+        <label>married</label>
+        <input checked={""} type="checkbox" className="married" />
+        <br />
+        <label>image</label>
+        <input value={""} type="text" className="image" required />
+        <br />
+        <input className="submitBtn" type="submit" />
+      </form>
+    </div>
+  );
+};
